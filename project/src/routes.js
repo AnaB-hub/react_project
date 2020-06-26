874b144681bdd3cbfb1ee8d025ab8f873bca2c17
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-26 09:17:07 -0300

    Retira component de Cadastro

--- project/src/routes.js
+++ project/src/routes.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-
 import Home from "./pages/Home/home";
-import CadastroUser from "./pages/User/CadastroUser";
 
 function Routes() {
   return (
