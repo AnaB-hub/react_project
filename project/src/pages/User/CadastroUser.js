1f68295a8bdad5567e6f0262f65f5a0d026ac4e5
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-26 09:59:32 -0300

    Adiciona página de cadastro de usuário

---
+++ project/src/pages/User/CadastroUser.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CadastroUser() {
+  return (
+    <div>
+      <h1>Cadastro User</h1>
+    </div>
+  );
+}
+
+export default CadastroUser;
--- project/src/routes.js
+++ project/src/routes.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+
 import Home from "./pages/Home/home";
+import CadastroUser from "./pages/User/CadastroUser";
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/cadastro-usuario" component={CadastroUser} />
     </Switch>
   );
 }
