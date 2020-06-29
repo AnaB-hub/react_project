2c081c010d1014e997579f634ea3daaa37c20a57
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-29 11:55:17 -0300

    Adiciona texto ao componente de página inicial

--- project/src/pages/Home/home.js
+++ project/src/pages/Home/home.js
@@ -3,8 +3,62 @@ import "./home.css";
 
 function Home() {
   return (
-    <div>
-      <h1>Página Inicial</h1>
+    <div className="body">
+      <div className="titulo">
+        <h1>Project</h1>
+      </div>
+      <div className="text">
+        <p>
+          O presente projeto possui por objetivo realizar um sistema completo
+          utilizando reactJs, react hooks, redux e redux saga. O sistema
+          fornecerá dicas de como economizar, organizar suas finanças e fazer
+          renda extra, com base nas respostas que o usuário fornecer. O projeto
+          ainda não possui um nome oficial, por isso, estaremos chamando apenas
+          de "Project".
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique
+          porta molestie. Proin vitae elit nisi. Cras nec auctor mi, ac
+          tincidunt est. Suspendisse ut massa non diam feugiat molestie rutrum
+          at massa. Nam porta sem nec ligula imperdiet congue at nec urna. Donec
+          cursus pulvinar lacus, quis volutpat felis varius bibendum.
+          Suspendisse potenti. Suspendisse elementum urna auctor libero commodo,
+          at luctus massa vestibulum. Mauris nibh lectus, finibus ut luctus vel,
+          scelerisque at quam. Morbi vel tincidunt tellus, in iaculis mi. Sed in
+          ex ultricies, ornare odio in, dignissim nisi. Pellentesque accumsan
+          magna vitae lacus egestas hendrerit. Quisque risus nisl, posuere in
+          purus eu, lobortis ornare diam. Donec odio magna, suscipit id lorem
+          ut, placerat sagittis lorem.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique
+          porta molestie. Proin vitae elit nisi. Cras nec auctor mi, ac
+          tincidunt est. Suspendisse ut massa non diam feugiat molestie rutrum
+          at massa. Nam porta sem nec ligula imperdiet congue at nec urna. Donec
+          cursus pulvinar lacus, quis volutpat felis varius bibendum.
+          Suspendisse potenti. Suspendisse elementum urna auctor libero commodo,
+          at luctus massa vestibulum. Mauris nibh lectus, finibus ut luctus vel,
+          scelerisque at quam. Morbi vel tincidunt tellus, in iaculis mi. Sed in
+          ex ultricies, ornare odio in, dignissim nisi. Pellentesque accumsan
+          magna vitae lacus egestas hendrerit. Quisque risus nisl, posuere in
+          purus eu, lobortis ornare diam. Donec odio magna, suscipit id lorem
+          ut, placerat sagittis lorem.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique
+          porta molestie. Proin vitae elit nisi. Cras nec auctor mi, ac
+          tincidunt est. Suspendisse ut massa non diam feugiat molestie rutrum
+          at massa. Nam porta sem nec ligula imperdiet congue at nec urna. Donec
+          cursus pulvinar lacus, quis volutpat felis varius bibendum.
+          Suspendisse potenti. Suspendisse elementum urna auctor libero commodo,
+          at luctus massa vestibulum. Mauris nibh lectus, finibus ut luctus vel,
+          scelerisque at quam. Morbi vel tincidunt tellus, in iaculis mi. Sed in
+          ex ultricies, ornare odio in, dignissim nisi. Pellentesque accumsan
+          magna vitae lacus egestas hendrerit. Quisque risus nisl, posuere in
+          purus eu, lobortis ornare diam. Donec odio magna, suscipit id lorem
+          ut, placerat sagittis lorem.
+        </p>
+      </div>
     </div>
   );
 }
