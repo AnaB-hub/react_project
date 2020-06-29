da2c2903597704a98f908c63e1978830d4301dc3
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-29 13:46:32 -0300

    Adiciona rodapé ao sistema

---
+++ project/src/components/rodape/rodape.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+
+import "./rodape.css";
+
+function Rodape() {
+  return (
+    <MDBFooter color="blue" className="font-small pt-4 mt-4 footer">
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright:{" "}
+          <a href="https://github.com/AnaB-hub/react_project">
+            {" "}
+            Ana Beatriz Garcia{" "}
+          </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
+  );
+}
+
+export default Rodape;
