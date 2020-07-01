19fefe12ad510515f766f36516b7236dbc8ab180
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-07-01 11:37:52 -0300

    Adiciona comentário para consulta futura

--- project/src/store/modules/user/sagas.js
+++ project/src/store/modules/user/sagas.js
@@ -1,6 +1,8 @@
 import { all, takeLatest } from "redux-saga/effects";
 
 function* addUserToLocalStorage({ user }) {
+  // Criado apenas para ter um exemplo de como deve-se fazer
+  // O saga é para requisições
   yield localStorage.setItem("user", user);
 }
 
